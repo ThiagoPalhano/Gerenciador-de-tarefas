44cddb194e97c35d539b008a0c3e1516994add40
Thiago Palhano <thiagolpalhano@gmail.com>
2025-05-30 11:34:44 -0300

    métodos e classes atualizados

--- Model/usuarios.py
+++ Model/usuarios.py
@@ -1,5 +1,5 @@
 from typing import Optional, List
-from Model.gerenciador_dao import GerenciadorDao
+from gerenciador_dao import GerenciadorDao
 
 class Usuario:
     dao: Optional[GerenciadorDao] = None
